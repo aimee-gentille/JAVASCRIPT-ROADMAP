758e051d9ac8484a14c7dc67ed9dd41400370ee4
Aimee Gentille <iradukundaimegentille@gmail.com>
2026-05-19 20:16:44 +0200

    everything we learn in javascript + today's class of datatypes

---
+++ DAY-02/exercise.js
@@ -0,0 +1,67 @@
+let name= "Gentille";
+let age= 20;
+let country= "Rwanda";
+let loveJavaScript = true;
+
+
+//question2 
+let score= 50;
+
+score=30;
+score=40;
+
+//question 3
+const PI= 3.14;
+
+console.log(PI);
+
+//question 4
+let firstName= "Gentille";
+let user_age= 20;
+let $price= 2000;
+
+console.log(typeof name);              
+console.log(typeof age);               
+console.log(typeof country);           
+console.log(typeof loveJavaScript);
+
+
+console.log(name+" "+country);
+
+let priceOfAnItem = 20000;
+let quantity = 2;
+
+let total = priceOfAnItem * quantity;
+
+console.log(total);
+
+let isLoggedIn= true;
+let isAdmin= true;
+let hasPermission= false;
+
+console.log("User is Logged In:"+" "+ isLoggedIn);
+console.log("User is Admin:"+" "+ isAdmin);
+console.log("User has Permission:"+" "+ hasPermission);
+
+let favFood=["Pasta", "Chips", "Chicken"];
+console.log(favFood);
+
+favFood.push("Salad");
+console.log(favFood);
+
+favFood.splice(1,1);
+console.log(favFood);
+
+favFood.pop(1,1);
+console.log(favFood);
+
+
+
+            
+
+
+
+
+
+
+
